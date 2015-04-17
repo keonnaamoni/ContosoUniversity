43ae8341d6282aef5ed840824cb31ac8c0d4ac9d
keonnaamoni <keonnaevans@gmail.com>
2015-04-17 15:49:59 -0400

    Created code for Database SchoolContext

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,13 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(): base("SchoolContext")
+        {
+        }
+    }
+}
