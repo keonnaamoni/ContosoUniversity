62f934706197849036f3418ac36c802b24c195eb
keonnaamoni <keonnaevans@gmail.com>
2015-04-17 15:52:53 -0400

    Added additional code to SchoolContext for the database

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -9,5 +9,16 @@ namespace ContosoUniversity.DAL
         public SchoolContext(): base("SchoolContext")
         {
         }
-    }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+{
+modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+}
+}
+}
+
+    
+
 }
