a7adc73eeca3ab430d782cb25eb6adeb40dd488b
keonnaamoni <keonnaevans@gmail.com>
2015-04-27 14:43:45 -0400

    Add Migration Complex Data Model

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿using System.ComponentModel.DataAnnotations;
 namespace ContosoUniversity.Models
 {
     public enum Grade
@@ -14,6 +10,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
         public virtual Course Course { get; set; }
         public virtual Student Student { get; set; }
