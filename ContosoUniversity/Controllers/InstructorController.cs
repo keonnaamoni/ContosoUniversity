433b0f167188e3d7f202b86c1e3c337d559f12a2
keonnaamoni <keonnaevans@gmail.com>
2015-04-27 15:43:40 -0400

    Added InstructorIndexData Class and InstructorController

---
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Instructor
+        public async Task<ActionResult> Index()
+        {
+            var instructors = db.Instructors.Include(i => i.OfficeAssignment);
+            return View(await instructors.ToListAsync());
+        }
+
+        // GET: Instructor/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instructor instructor = await db.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructor/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location");
+            return View();
+        }
+
+        // POST: Instructor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Instructors.Add(instructor);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
+            return View(instructor);
+        }
+
+        // GET: Instructor/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instructor instructor = await db.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
+            return View(instructor);
+        }
+
+        // POST: Instructor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
+            return View(instructor);
+        }
+
+        // GET: Instructor/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instructor instructor = await db.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: Instructor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Instructor instructor = await db.Instructors.FindAsync(id);
+            db.Instructors.Remove(instructor);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
